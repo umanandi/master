75164b0f915a46eb9aba329a30c19e2897d8a297
uma-nandi <uma.nandi@hpe.com>
2018-03-01 11:23:36 -0800

    initial version

---
+++ test-service/src/main/java/com/test/Test.java
@@ -0,0 +1,17 @@
+package com.test;
+
+/**
+ *
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        log("Testing ");
+    }
+
+
+    private static void log(String message) {
+        System.out.println(message);
+    }
+}
